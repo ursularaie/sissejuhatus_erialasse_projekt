ffbdcb13beb34dd41358947ab44e7b2ef53c35c7
ursularaie <ursula.raie@gmail.com>
2025-11-26 17:05:57 +0200

    muudtatused

---
+++ script.js
@@ -0,0 +1,6 @@
+//quiz, kui saab valesti saab vajutada nuppu proovi uuesti
+//lisa lõpptulemuste kast, kui kõik küsimused on vastatud ja vajutatkse vastavat nuppu
+//15 laulu, valib iga küsimuse jaoks suvakjalt ühe kolmest, mis on seal vastuses
+//
+
+console.log("script.js on laetud");
